84d8dc024d523d8149a12c38ba99cfd19dab5e2b
RSedgwick <ruby@sedgy.net>
2023-05-31 16:17:41 +0100

    plots for optimisation inits

--- model_learning_curves/analysis_utils.py
+++ model_learning_curves/analysis_utils.py
@@ -28,3 +28,31 @@ def load_learning_curve_results():
                             results_list.append(df)
     results_df = pd.concat(results_list, ignore_index=True)
     return results_df
+
+def load_all_hyperparameters():
+    """Load the results of the learning curves for all the different models and return it as a dataframe
+    :return:
+    """
+
+    path = pl.Path.home() / 'Transfer_Learning_GP_Results/hyperparameters'
+    n_new_fun = 10
+    surface_types = ['unrelated', 'linear_relation', 'non-linear_relation']
+    results_list = []
+    test_type = f'two_observed_{n_new_fun}_new'
+    for n_new_points in range(2, 30):
+        for surface_type in surface_types:
+            for seed in [1, 2, 3]:
+                for data_seed in range(6):
+                    if os.path.exists(
+                            path / f'hyperparameters_{surface_type}_{test_type}_{n_new_points}_points_seed_{seed}_dataseed_{data_seed}.pkl'):
+                        with open(
+                                path / f'hyperparameters_{surface_type}_{test_type}_{n_new_points}_points_seed_{seed}_dataseed_{data_seed}.pkl',
+                                'rb') as f:
+                            df = pickle.load(f)
+                            df['n_new_points'] = n_new_points
+                            df['seed'] = seed
+                            df['data_seed'] = data_seed
+                            df['surface_type'] = surface_type
+                            results_list.append(df)
+    hyp_df = pd.concat(results_list, ignore_index=True)
+    return hyp_df
--- model_learning_curves/plotting_utils.py
+++ model_learning_curves/plotting_utils.py
@@ -82,8 +82,9 @@ def plot_all_models_from_hyperparameters(model_dict, hyperparams, plot_new_ys=Tr
     """Plot all the models from the hyperparameters in the dictionary"""
 
     n_fun = len(np.unique(hyperparams['fun_nos'][0]))
+    mod_names = {'mo_indi': 'MOGP', 'lmc': 'LMC', 'avg': 'AvgGP', 'lvmogp': 'LVMOGP'}
 
-    fig, axs = plt.subplots(nrows=len(model_dict), ncols=1, figsize=(20, 12))
+    fig, axs = plt.subplots(nrows=len(model_dict), ncols=1, figsize=(25, 12))
 
     for ax in axs:
         ax.remove()
@@ -96,10 +97,104 @@ def plot_all_models_from_hyperparameters(model_dict, hyperparams, plot_new_ys=Tr
         mod_df = hyperparams[hyperparams['model'] == model_name]
 
         subfig = subfigs[row]
-        subfig.suptitle(f'{model_name}')
+        subfig.suptitle(f'{mod_names[model_name]}')
         subfig = plot_model_from_hyperparameters(model, mod_df, subfig, plot_new_ys=plot_new_ys)
         row += 1
 
-    plt.tight_layout()
-
     return fig
+
+def plot_learning_curve_results(results_df, seeds, mean=False):
+    """Plot the RMSE and the NLPD against iteration from the results dataframe"""
+    model_names = ['mo_indi', 'avg', 'lmc', 'lvmogp', ]
+    colors = get_colors()
+
+    surface_type_names = {'unrelated': 'Uncorrelated',
+                          'linear_relation': 'Linearly\nCorrelated',
+                          'non-linear_relation': 'Non-Linearly\nCorrelated'}
+
+    mod_names = {'mo_indi': 'MOGP', 'lmc': 'LMC', 'avg': 'AvgGP', 'lvmogp': 'LVMOGP'}
+    metric_names = {'rmse_new_surface': 'RMSE', 'nlpd_new_surface': 'NLPD'}
+
+    cm = 1 / 2.54
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(17 * cm, 4))
+    surface_types = ['unrelated', 'linear_relation', 'non-linear_relation']
+
+    for i, surface_type in enumerate(surface_types):
+        for j, metric in enumerate(['rmse_new_surface', 'nlpd_new_surface']):
+            for k, model_name in enumerate(model_names):
+                if mean:
+                    temp_df_ = results_df[(results_df['surface_type'] == surface_type)
+                                          & (results_df['model'] == model_name) & (results_df['seed'].isin(seeds))]
+                    temp_df = temp_df_.groupby('n_new_points').mean().reset_index()
+                    label = mod_names[model_name]
+                    axs[j, i].plot(temp_df['n_new_points'], temp_df[metric], alpha=0.8, color=colors[model_name],
+                                   linewidth=1, label=label)
+
+                else:
+                    for data_seed in results_df['data_seed'].unique():
+                        temp_df = results_df[(results_df['surface_type'] == surface_type)
+                                             & (results_df['model'] == model_name) & (results_df['seed'].isin(seeds))
+                                             & (results_df['data_seed'] == data_seed)]
+                        if data_seed == 1:
+                            label = mod_names[model_name]
+                        else:
+                            label = None
+                        axs[j, i].plot(temp_df['n_new_points'], temp_df[metric], alpha=0.8, color=colors[model_name],
+                                       linewidth=1, label=label)
+
+            axs[j, 0].set_ylabel(metric_names[metric])
+        axs[0, i].set_title(surface_type_names[surface_type])
+        axs[1, i].set_ylim(-2.5, 10)
+        axs[0, i].set_ylim(0, 1)
+    axs[1, 1].set_xlabel('number of points on new functions')
+    axs[1, 1].legend(bbox_to_anchor=(0.5, -0.65), loc='lower center', ncol=4)
+
+def get_colors():
+    """Get the colors for the different models so they are the same in all plots"""
+    cols = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    colors = {'lmc': cols[0], 'mo_indi': cols[3], 'lvmogp': cols[1], 'avg': cols[2]}
+    return colors
+
+def plot_log_marginal_likelihoods_of_restarts(hyp_df, seed, data_seed):
+    """Plot the log marginal likelihoods of the different restarts of the hyperparameters"""
+
+    surface_type_names = {'unrelated': 'Uncorrelated',
+                          'linear_relation': 'Linearly\nCorrelated',
+                          'non-linear_relation': 'Non-Linearly\nCorrelated'}
+
+    surface_types = ['unrelated', 'linear_relation', 'non-linear_relation']
+
+    mod_names = {'mo_indi': 'MOGP', 'lmc': 'LMC', 'avg': 'AvgGP', 'lvmogp': 'LVMOGP'}
+
+    labels = {'mo_indi': {'random restart 1': 'random', 'random restart 2': 'random', 'random restart 3': 'random'},
+              'lmc': {'random_W_and_k': 'both W and $kappa$ random',
+                      'random_W_and_k_0': 'W random and $kappa=0$',
+                      'random_W_and_k_1': 'W random and $kappa=1$'},
+              'lvmogp': {
+                  'random': 'random',
+                  'gpy': 'gpy',
+                  'mo_pca': 'PCA'},
+              'avg': {'random restart 1': 'random', 'random restart 2': 'random', 'random restart 3': 'random'}}
+
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    for model_name in ['mo_indi', 'avg', 'lmc', 'lvmogp']:
+        fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(17, 6.2))
+        ax = axs.flatten()
+        for k, surface_type in enumerate(surface_types):
+            hyp_df_model = hyp_df[
+                (hyp_df['model'] == model_name) & (hyp_df['seed'] == seed) & (hyp_df['data_seed'] == data_seed)
+                & (hyp_df['surface_type'] == surface_type)]
+            for i, init_name in enumerate(hyp_df_model['init_type'].unique()):
+                for restart in hyp_df_model['restart'].unique():
+                    df = hyp_df_model[(hyp_df_model['init_type'] == init_name)
+                                      & (hyp_df_model['restart'] == restart)]
+                    ax[k].plot(df['n_new_points'], df['final_lml'], color=colors[i],
+                               label=labels[model_name][init_name])
+            ax[k].set_title(surface_type_names[surface_type])
+            ax[k].set_ylabel('log marginal likelihood')
+            ax[k].set_xlabel('no. new points')
+            if model_name == 'lvmogp':
+                ax[k].set_ylim(-150, 500)
+        plt.suptitle(mod_names[model_name])
+        plt.legend()
+        plt.show()
